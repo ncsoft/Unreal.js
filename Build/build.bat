c6d3127b47492a87167e6b31e7830b771606b059
Nako Sung <deif79@gmail.com>
2016-01-06 19:42:26 +0900

    Build script

--- Build/build.bat
+++ Build/build.bat
@@ -1,11 +1,11 @@
-powershell.exe -nologo -noprofile -command "& { $x = (Get-ItemProperty \"hklm:\Software\Classes\Unreal.ProjectFile\shell\open\command\").\"(default)\"; $x = $x.SubString(0,$x.IndexOf(\"Launcher\")) + \"4.10\"; if ($x[0] -eq '\"') { $x.SubString(1) } else { $x } }" > temp.txt
+powershell.exe -nologo -noprofile -command "& { $x = (Get-ItemProperty \"hklm:\Software\Classes\Unreal.ProjectFile\shell\open\command\").\"(default)\"; $y = $x.IndexOf(\"Launcher\"); if ($y -gt 0) { $x = $x.SubString(0,$y) + \"4.10\"; } else { $x = $x.SubString(0,$x.IndexOf(\"Binaries\")-1); }; if ($x[0] -eq '\"') { $x.SubString(1) } else { $x } }" > temp.txt
 for /F "usebackq delims=", %%g in (`type temp.txt`) do set UE4PATH=%%g
 del temp.txt
 
 call "%UE4PATH%\Engine\Build\BatchFiles\RocketGenerateProjectFiles.bat" %~dp0\Build.uproject -game
 
 
-rem call "%VS140COMNTOOLS%"\vsvars32.bat
+call "%VS140COMNTOOLS%"\vsvars32.bat
 xcopy ..\Plugins .\Plugins /s /d /k /r /y /i
 msbuild build.sln /p:Configuration="Development Editor" /p:Platform=Win64
 xcopy Plugins\UnrealJS\Binaries\*.dll ..\Plugins\UnrealJS\Binaries /s /d /k /y /i
