02f5efc714385a27993beff4e68b1e500eff9057
Nako Sung <deif79@gmail.com>
2016-01-14 06:01:13 +0900

    No shipping build

--- Build/build.bat
+++ Build/build.bat
@@ -8,12 +8,12 @@ call "%UE4PATH%\Engine\Build\BatchFiles\RocketGenerateProjectFiles.bat" %~dp0\Bu
 call "%VS140COMNTOOLS%"\vsvars32.bat
 xcopy ..\Plugins .\Plugins /s /d /k /r /y /i
 msbuild build.sln /p:Configuration="Development Editor" /p:Platform=Win64
-msbuild build.sln /p:Configuration="Shipping" /p:Platform=Win64
 REM goto exit
 xcopy Plugins\UnrealJS\Binaries\*.dll ..\Plugins\UnrealJS\Binaries /s /d /k /y /i
 
 rd /s/q Staging
 xcopy Plugins\UnrealJS\Binaries\*.dll Staging\Plugins\UnrealJS\Binaries /s /d /k /r /y /i
+xcopy Plugins\UnrealJS\Binaries\UE4-*-Shipping.lib Staging\Plugins\UnrealJS\Binaries /s /d /k /r /y /i
 xcopy Plugins\UnrealJS\Content Staging\Plugins\UnrealJS\Content /s /d /k /r /y /i
 xcopy Plugins\UnrealJS\Resources Staging\Plugins\UnrealJS\Resources /s /d /k /r /y /i
 copy Plugins\UnrealJS\* Staging\Plugins\UnrealJS 
