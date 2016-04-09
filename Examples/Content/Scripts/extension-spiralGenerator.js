8e7cc1765490690a6af5863f9dbca30d46cb3e45
Nako Sung <deif79@gmail.com>
2016-04-09 10:25:01 +0900

    extension example updated

--- Examples/Content/Scripts/extension-spiralGenerator.js
+++ Examples/Content/Scripts/extension-spiralGenerator.js
@@ -1,5 +1,70 @@
+const tags = ["PCG"]
+	
+function purge(world) {
+    let prev_actors = world.GetAllActorsOfClassAndTags(StaticMeshActor, tags).OutActors  
+    prev_actors.forEach((actor) => world.EditorDestroyActor(actor))  
+}
+    
+function generate_spiral(world, opts) {
+    const mesh = StaticMesh.Load('/Engine/BasicShapes/Sphere')
+    const mtrl = Material.Load('/Game/Color.Color')
+    
+    let N = opts.N || 10
+    let num_spirals = opts.num_spirals || 5
+    let radius = opts.radius || 200
+    let height = opts.height || 200 * 5
+    purge(world)		
+    for (let i = 0; i < N; ++i) {
+        let v = i / N
+        let u = v * num_spirals * 2 * Math.PI
+        let t = {
+            Translation: {
+                X: Math.sin(u) * radius,
+                Y: Math.cos(u) * radius,
+                Z: v * height
+            }
+        }
+        let color = {
+            R: t.Translation.X * 2 + 1,
+            G: t.Translation.Y * 2 + 1,
+            B: v, 
+            A: 1
+        } 
+        let mi = world.CreateDynamicMaterialInstance(mtrl)
+        mi.SetVectorParameterValue('color', color)
+        let sma = StaticMeshActor.C(world.BeginSpawningActorFromClass(StaticMeshActor, t, true))
+        sma.StaticMeshComponent.StaticMesh = mesh			
+        sma.StaticMeshComponent.SetMaterial(0, mi)
+        sma.StaticMeshComponent.ReregisterComponent()
+        sma.FinishSpawningActor(t)
+        sma.Tags = tags
+    }
+}
+
+function get_engine() {
+    return Root.GetEngine()
+}
+
+function get_world() {
+    return get_engine().GetEditorWorld()
+}
+
+function redraw() {
+    get_engine().RedrawAllViewports(true)
+}
+    
+function generate(data) {
+    generate_spiral(get_world(), data)
+    redraw()
+}
+
+function clear() {
+    purge(get_world())
+    redraw()
+}    
+
 function main() {
-	let UMG = require('UMG')
+    let UMG = require('UMG')
 	let instantiator = require('instantiator')
 	
 	let schema = {
@@ -45,68 +110,43 @@ function main() {
 		return require('uclass')()(global,eval(code))
 	}
 	let meta = create_meta(schema)
-		
-	function generate_spiral(world, opts) {
-		const mesh = StaticMesh.Load('/Engine/BasicShapes/Sphere')
-		const mtrl = Material.Load('/Game/Color.Color')
-		const tags = ["PCG"]
-
-		let N = opts.N || 10
-		let num_spirals = opts.num_spirals || 5
-		let radius = opts.radius || 200
-		let height = opts.height || 200 * 5
-		purge(world)		
-		for (let i = 0; i < N; ++i) {
-			let v = i / N
-			let u = v * num_spirals * 2 * Math.PI
-			let t = {
-				Translation: {
-					X: Math.sin(u) * radius,
-					Y: Math.cos(u) * radius,
-					Z: v * height
-				}
-			}
-			let color = {
-				R: t.Translation.X * 2 + 1,
-				G: t.Translation.Y * 2 + 1,
-				B: v, 
-				A: 1
-			} 
-			let mi = world.CreateDynamicMaterialInstance(mtrl)
-			mi.SetVectorParameterValue('color', color)
-			let sma = StaticMeshActor.C(world.BeginSpawningActorFromClass(StaticMeshActor, t, true))
-			sma.StaticMeshComponent.StaticMesh = mesh			
-			sma.StaticMeshComponent.SetMaterial(0, mi)
-			sma.StaticMeshComponent.ReregisterComponent()
-			sma.FinishSpawningActor(t)
-			sma.Tags = tags
-		}
-	}
-		
-	let data = new meta()
-	data.num_spirals = 10;
-	data.radius = 320; 
-	data.N = 100;
-	data.height = 1000
-	
-	function generate() {
-		let world = Root.GetEngine().GetEditorWorld()
-		generate_spiral(world, data)
-		Root.GetEngine().RedrawAllViewports(true)
-	}
-	
-	function clear() {
-		let world = Root.GetEngine().GetEditorWorld()
-		purge(world)
-		Root.GetEngine().RedrawAllViewports(true)
-	}    
-	
+    
+    let data = new meta()
+    data.num_spirals = 10;
+    data.radius = 320; 
+    data.N = 100;
+    data.height = 1000
+    
+    let GEngine = Root.GetEngine()    
+    const buttonTextStyle = {
+        Font : {
+            FontObject : GEngine.SmallFont,
+            Size : 10
+        },
+        ColorAndOpacity : {
+            SpecifiedColor:{R:0,G:0,B:0,A:1}
+        }
+    }
 	let design = UMG.div({},
 		UMG(PropertyEditor,{$link:elem => {
 			elem.SetObject(data)
 		}}),
-		UMG(Button,{OnClicked:generate},UMG.text({},"Generate!")),
-		UMG(Button,{OnClicked:clear},UMG.text({},"Purge"))
+        UMG.span({},
+            UMG(Button,
+                {
+                    OnClicked:_ => generate(data),
+                    ToolTipText:'Generate spirals on editor world'
+                },
+                UMG.text(buttonTextStyle,"Generate!!")
+            ),
+            UMG(Button,
+                {
+                    OnClicked:clear,
+                    ToolTipText:'Purge last created spirals'
+                },
+                UMG.text(buttonTextStyle,"Purge")
+            )
+        )		
 	)
 	return instantiator(design)
 }
