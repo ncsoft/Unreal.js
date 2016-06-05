cc8a2172e7cd1713a6c6d53bd81f4254e92c9bf9
Nako Sung <deif79@gmail.com>
2016-06-05 11:30:05 +0900

    Update helloInputBinding.js

--- Examples/Content/Scripts/helloInputBinding.js
+++ Examples/Content/Scripts/helloInputBinding.js
@@ -1,58 +1,56 @@
 /// <reference path="typings/ue.d.ts">/>
-(function (global) {
-    "use strict"
+"use strict"
 
-    // To specify binding flag, pass '-flag' or '+flag' as a decorator.
-    //
-    // eg) AxisBinding[Turn,-bConsumeInput]
-    //
-    // FBlueprintInputDelegateBinding
-    //     bConsumeInput, bExecuteWhenPaused, bOverrideParentBinding
+// To specify binding flag, pass '-flag' or '+flag' as a decorator.
+//
+// eg) AxisBinding[Turn,-bConsumeInput]
+//
+// FBlueprintInputDelegateBinding
+//     bConsumeInput, bExecuteWhenPaused, bOverrideParentBinding
 
-    class MyPlayerController extends PlayerController  {
-        Wave() /*ActionBinding[Wave]*/ {
-            console.log("Wave!!")
-        }
+class MyPlayerController extends PlayerController  {
+    Wave() /*ActionBinding[Wave]*/ {
+        console.log("Wave!!")
+    }
 
-        Jump() /*ActionBinding[Jump2,IE_Released]*/ {
-            console.log("Jump!!")
-        }
+    Jump() /*ActionBinding[Jump2,IE_Released]*/ {
+        console.log("Jump!!")
+    }
 
-        Hi() /*KeyBinding[Ctrl+E]*/ {
-            console.log("Hi!")
-        }
+    Hi() /*KeyBinding[Ctrl+E]*/ {
+        console.log("Hi!")
+    }
 
-        Turn(value/*float*/) /*AxisBinding[Turn,-bConsumeInput]*/ {
-            if (value) {
-                console.log("Turn2",value)
-            }
+    Turn(value/*float*/) /*AxisBinding[Turn,-bConsumeInput]*/ {
+        if (value) {
+            console.log("Turn2",value)
         }
     }
+}
 
-    function GetPC() {
-        return GWorld.GetAllActorsOfClass(PlayerController).OutActors[0]
-    }
+function GetPC() {
+    return GWorld.GetAllActorsOfClass(PlayerController).OutActors[0]
+}
 
-    // Switch PlayerController class to JS generated one.
-    let MyPC_C = require('uclass')()(global,MyPlayerController)
-    GWorld.GetGameMode().PlayerControllerClass = MyPC_C
+// Switch PlayerController class to JS generated one.
+let MyPC_C = require('uclass')()(global,MyPlayerController)
+GWorld.GetGameMode().PlayerControllerClass = MyPC_C
 
-    if (GetPC()) {
-        GWorld.GetGameMode().HandleSeamlessTravelPlayer(GetPC())
-    }
+if (GetPC()) {
+    GWorld.GetGameMode().HandleSeamlessTravelPlayer(GetPC())
+}
 
-    function main() {
-        return () => {}
-    }
+function main() {
+    return () => {}
+}
 
-    try {
-        module.exports = () => {
-            let cleanup = null
-            process.nextTick(() => cleanup = main());
-            return () => cleanup()
-        }
+try {
+    module.exports = () => {
+        let cleanup = null
+        process.nextTick(() => cleanup = main());
+        return () => cleanup()
     }
-    catch (e) {
-        require('bootstrap')('helloInputBinding')
-    }
-})(this)
+}
+catch (e) {
+    require('bootstrap')('helloInputBinding')
+}
