0d27ab04e201775df8632e0bc8e9c9c8bfeba221
Nako Sung <deif79@gmail.com>
2016-06-05 11:29:49 +0900

    Update helloGame.js

--- Examples/Content/Scripts/helloGame.js
+++ Examples/Content/Scripts/helloGame.js
@@ -1,58 +1,56 @@
 /// <reference path="typings/ue.d.ts">/>
-(function (global) {
-    "use strict"
+"use strict"
+
+class MyPC extends PlayerController {
+    ReceiveBeginPlay() {
+        super.ReceiveBeginPlay()
+
+        console.log("Hello, this is MyPC")
+    }
     
-    class MyPC extends PlayerController {
-        ReceiveBeginPlay() {
-            super.ReceiveBeginPlay()
-
-            console.log("Hello, this is MyPC")
-        }
-        
-        // RPC function!
-        test(data/*float*/)/*NetMulticast*/ {
-            console.log('multicast!',data,this.GetName(),this.Role)
-        }
-        
-        TestBinding()/*KeyBinding[Ctrl+E]*/{
-           this.Server_Call();
-        }   
-        
-        Server_Call()/*Server*/ {
-            console.log('this is server call',this.Role);
-            this.test($time);
-        }     
+    // RPC function!
+    test(data/*float*/)/*NetMulticast*/ {
+        console.log('multicast!',data,this.GetName(),this.Role)
     }
-
-    let MyPC_C = require('uclass')()(global,MyPC)
     
-
-    let _ = require('lodash')
-
-    function main() {
-        // Replicated actor should be created in server node.
-        if (GWorld.IsServer()) {
+    TestBinding()/*KeyBinding[Ctrl+E]*/{
+       this.Server_Call();
+    }   
     
-            Root.GetOuter().PlayerControllerClass = MyPC_C
+    Server_Call()/*Server*/ {
+        console.log('this is server call',this.Role);
+        this.test($time);
+    }     
+}
 
-            let alive = true
+let MyPC_C = require('uclass')()(global,MyPC)
 
-            return function () {
-                alive = false
-            }
-        } else {
-            return function() {}
+
+let _ = require('lodash')
+
+function main() {
+    // Replicated actor should be created in server node.
+    if (GWorld.IsServer()) {
+
+        Root.GetOuter().PlayerControllerClass = MyPC_C
+
+        let alive = true
+
+        return function () {
+            alive = false
         }
+    } else {
+        return function() {}
     }
+}
 
-    try {
-        module.exports = () => {
-            let cleanup = null
-            cleanup = main();
-            return () => cleanup()
-        }
+try {
+    module.exports = () => {
+        let cleanup = null
+        cleanup = main();
+        return () => cleanup()
     }
-    catch (e) {
-        require('bootstrap')('helloGame')
-    }
-})(this)
+}
+catch (e) {
+    require('bootstrap')('helloGame')
+}
