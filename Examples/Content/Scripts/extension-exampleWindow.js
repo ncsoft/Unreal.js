1bbbc706e3d82884cbaaadb4a06763c2b633d291
Nako Sung <deif79@gmail.com>
2016-06-06 02:35:47 +0900

    Update extension-exampleWindow.js

--- Examples/Content/Scripts/extension-exampleWindow.js
+++ Examples/Content/Scripts/extension-exampleWindow.js
@@ -1,6 +1,11 @@
 /// <reference path="typings/ue.d.ts" />
 "use strict"
 
+const repositoryUrl = 'https://raw.githubusercontent.com/ncsoft/Unreal.js-packages/master/repository.json'
+const githubUrl = 'https://api.github.com/repos/ncsoft/Unreal.js'
+const homepageUrl = "https://github.com/ncsoft/Unreal.js"
+const svnPath = `${Context.GetDir('Engine')}/Binaries/ThirdParty/svn/Win64/svn`
+
 function main() {
     const UMG = require('UMG')
     let {EventEmitter} = require('events')
@@ -10,15 +15,52 @@ function main() {
         FontObject:Root.GetEngine().SmallFont
     }
     
+    const I = require('instantiator')
+    const _ = require('lodash')
     const request = require('request')
     const style = new JavascriptStyleSet
     style.StyleSetName = 'EditorStyle'
+    
+    let workDir = Context.GetDir('GameContent')+'/Scripts/Downloaded'
+    
+    function fetchGithub() {
+        return request('GET',githubUrl)
+    }
+    
+    function fetchPackages() {
+        return request("GET",repositoryUrl)
+    }
+    
+    function getNumClassesExported() {
+        return _.filter(_.keys(global),x=>!!global[x].StaticClass).length
+    }
+    
+    function installPackage(pack) {
+        return JavascriptProcess.Create(
+            svnPath,
+            `co ${pack.details}`,
+            false,
+            false,
+            false,0,workDir).Wait()
+    }
+    
+    function packageToObject(p) {
+        let o = new JavascriptObject()
+        o.package = p
+        let details = p.details
+        let split = details.lastIndexOf('/')
+        o.installed = false
+        if (split >= 0) {
+            o.installed = JavascriptLibrary.DirectoryExists(workDir + details.substr(split))
+        }
+        return o
+    }
      
     makeWindow("$window",
     {
         SizingRule:'AutoSized',  
-        Title:'Hello Unreal.js' 
-    })(finish => { 
+        Title:'Unreal.js' 
+    })(finish => {
         let E = new EventEmitter()
         return UMG(SizeBox,{WidthOverride:400},            
             UMG.div({Size:{Rule:'Fill'}},
@@ -31,7 +73,7 @@ function main() {
                         'Slot.VerticalAlignment':'VAlign_Center',
                         $link:elem => {
                             elem.alive = true
-                            request('GET','https://api.github.com/repos/ncsoft/Unreal.js').then(json => {
+                            fetchGithub().then(json => {
                                 const {stargazers_count} = json
                                 elem.SetText(`${stargazers_count} stars`)
                             })
@@ -41,56 +83,68 @@ function main() {
                         }
                     })
                 ),            
-                UMG.text({AutoWrapText:true,Font:font},
-`Unreal.js is a plug-in which brings V8-powered Javascript into UnrealEngine4.
-${_.filter(_.keys(global),x=>!!global[x].StaticClass).length} classes exported`
+                UMG.text({AutoWrapText:true,Font:font,'Slot.Padding':{Left:20,Top:10}},
+`Unreal.js is a plug-in which brings V8-powered Javascript into UnrealEngine4. 
+
+Copyright (c) 2016 NCSOFT Corporation
+
+${getNumClassesExported()} classes exported`
                 ),
-                UMG(SizeBox,{HeightOverride:100},
-                    UMG(ScrollBox,{
-                        $link:elem => {                        
-                            elem.alive = true
-                            let remaining = 100
-                            function tick() {
-                                if (!elem.alive) return
-                                if (remaining-- <= 0) return
-                                
-                                let item = Math.random()
-                                elem.add_child(
-                                    UMG(Button,
+                UMG(SizeBox,{HeightOverride:20}),
+                UMG(SizeBox,{HeightOverride:200},                
+                    UMG(JavascriptListView,{
+                        ItemHeight:20,
+                        OnGenerateRowEvent:(item,column) => {
+                            const isName = column == 'Name'
+                            return I(                                
+                                UMG.text(
                                     {
-                                        WidgetStyle: style.GetButtonStyle("HoverHintOnly"),
-                                        OnClicked:_ => E.emit('click',item)
+                                        Font:font,
+                                        ToolTipText: isName && item ? item.package.details : ''
                                     },
-                                        UMG.text({Font:font},`Random ${item}`))
-                                    )
-                                setTimeout(tick,100)
+                                    isName ?
+                                        (item ? item.package.name : 'Package name') :
+                                        (item ? item.installed ? "Installed" : "" : 'Status')
+                                )
+                            )
+                        },
+                        Columns:[
+                            {
+                                Id: 'Name',
+                                Width: 0.7
+                            },
+                            {
+                                Id: 'Status',
+                                Width: 0.3
                             }
-                            tick()
+                        ],
+                        $link:elem => {
+                            elem.JavascriptContext = Context
+                            elem.alive = true                        
+                            elem.proxy = {
+                                OnDoubleClick : item => {                                
+                                    JavascriptLibrary.MakeDirectory(workDir,false)
+                                    installPackage(item.package)                                    
+                                }
+                            }
+                            fetchPackages().then(repository => {
+                                if (!elem.alive) throw new Error("interrupted")
+                                elem.Items = repository.packages.map(packageToObject)
+                                elem.RequestListRefresh()
+                            })
                         },
                         $unlink:elem => {
                             elem.alive = false
                         }
-                    })                
-                ),        
-                UMG.div({ 
-                    $link:elem => {
-                        elem.handler = payload => {
-                            elem.add_child(UMG.text({Font:font},`clicked! ${payload}`))
-                        }
-                        E.on('click',elem.handler)
-                    },
-                    $unlink:elem => { 
-                        E.removeListener('click',elem.handler)
-                    }
-                }),
-                [1,2,3].map(x => UMG.text({Font:font},`count ${x}`)),
+                    })
+                ),
                 
                 UMG(Spacer,{'Slot.Size.Rule' : 'Fill'}),
                 UMG(Button, 
                     {   
                         WidgetStyle: style.GetButtonStyle("Credits.Button"),
                         OnClicked: _ => {
-                            JavascriptProcess.LaunchURL("https://github.com/ncsoft/Unreal.js")
+                            JavascriptProcess.LaunchURL(homepageUrl)
                         }
                     },
                     UMG.text({Font:font},"Visit project page")
