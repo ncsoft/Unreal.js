c3c89e98e3a9b5186dba30b156134ad78de333e4
Nako Sung <deif79@gmail.com>
2016-06-05 11:31:21 +0900

    Update helloRest.js

--- Examples/Content/Scripts/helloRest.js
+++ Examples/Content/Scripts/helloRest.js
@@ -1,31 +1,29 @@
-(function (global) {
-    "use strict"
+"use strict"
 
-    function main() {
-        let actor = new TextRenderActor(GWorld,{X:100,Z:100},{Yaw:180})
-        actor.TextRender.SetHorizontalAlignment('EHTA_Center')
-        actor.TextRender.SetText('Hello Rest')
+function main() {
+    let actor = new TextRenderActor(GWorld,{X:100,Z:100},{Yaw:180})
+    actor.TextRender.SetHorizontalAlignment('EHTA_Center')
+    actor.TextRender.SetText('Hello Rest')
 
-        let request = require('request')
-        request("POST","http://localhost:3000",{data:{json:'hello'}}).then((json) => {
-            actor.TextRender.SetText(JSON.stringify(json))
-        }).catch((error) => {
-            actor.TextRender.SetText(error)
-        })
+    let request = require('request')
+    request("POST","http://localhost:3000",{data:{json:'hello'}}).then((json) => {
+        actor.TextRender.SetText(JSON.stringify(json))
+    }).catch((error) => {
+        actor.TextRender.SetText(error)
+    })
 
-        return function () {
-            actor.DestroyActor()
-        }
+    return function () {
+        actor.DestroyActor()
     }
+}
 
-    try {
-        module.exports = () => {
-            let cleanup = null
-            process.nextTick(() => cleanup = main());
-            return () => cleanup()
-        }
+try {
+    module.exports = () => {
+        let cleanup = null
+        process.nextTick(() => cleanup = main());
+        return () => cleanup()
     }
-    catch (e) {
-        require('bootstrap')('helloRest')
-    }
-})(this)
+}
+catch (e) {
+    require('bootstrap')('helloRest')
+}
