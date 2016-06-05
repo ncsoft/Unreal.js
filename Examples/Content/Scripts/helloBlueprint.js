2d8c58053e884f7a06f7b4c76a8fc6071d2420b7
Nako Sung <deif79@gmail.com>
2016-06-05 11:28:49 +0900

    Update helloBlueprint.js

--- Examples/Content/Scripts/helloBlueprint.js
+++ Examples/Content/Scripts/helloBlueprint.js
@@ -1,72 +1,70 @@
 /// <reference path="typings/ue.d.ts">/>
 
-(function (global) {
-    "use strict"
+"use strict"
 
-    // Blueprint class can be subclassed!
-    class MyActor extends Blueprint.Load('/Game/ExampleBlueprint').GeneratedClass {
-        // constructor
-        ctor() {
-            // Subobject initialization, property initialization
-            this.bAlwaysRelevant = true
-        }
-
-        // declare UPROPERTY's here
-        // ; this.XXXXX/*[attribute+]+type*/;
-        properties() {
-            this.Hello/*Replicated+EditAnywhere+int*/;
-            this.World/*Replicated+EditAnywhere+Actor*/;
-            this.Position/*EditAnywhere+Vector[]*/;
-            this.Some/*EditAnywhere+DistanceDatum*/;
-        }
-
-        // Overriding function doesn't need function signature
-        ReceiveBeginPlay() {
-            super.ReceiveBeginPlay()
-
-            console.log("Hello, this is MyActor")
-        }
-
-        // Override an event which was declared in Blueprint
-        CustomEvent() {
-            console.log("This is javascript")
-        }
-
-        // New UFUNCTION needs proper function signature
-        // ; function-name(arg/*type*/,...) /*UFUNCTION-flag[+another flag]*/
-        NewFunction(x/*int*/,y/*int*/) /*NetMulticast*/ {
-            console.log(x+y);
-        }
+// Blueprint class can be subclassed!
+class MyActor extends Blueprint.Load('/Game/ExampleBlueprint').GeneratedClass {
+    // constructor
+    ctor() {
+        // Subobject initialization, property initialization
+        this.bAlwaysRelevant = true
     }
 
-    let MyActor_C = require('uclass')()(global,MyActor)
-
-    let _ = require('lodash')
-
-    function GetPC() {
-        return GWorld.GetAllActorsOfClass(PlayerController).OutActors[0]
+    // declare UPROPERTY's here
+    // ; this.XXXXX/*[attribute+]+type*/;
+    properties() {
+        this.Hello/*Replicated+EditAnywhere+int*/;
+        this.World/*Replicated+EditAnywhere+Actor*/;
+        this.Position/*EditAnywhere+Vector[]*/;
+        this.Some/*EditAnywhere+DistanceDatum*/;
     }
 
-    function main() {
-        if (GWorld.IsServer()) {
-            let actor = new MyActor_C(GWorld,{X:1})
+    // Overriding function doesn't need function signature
+    ReceiveBeginPlay() {
+        super.ReceiveBeginPlay()
 
-            return function () {
-                actor.DestroyActor()
-            }
-        } else {
-            return function() {}
+        console.log("Hello, this is MyActor")
+    }
+
+    // Override an event which was declared in Blueprint
+    CustomEvent() {
+        console.log("This is javascript")
+    }
+
+    // New UFUNCTION needs proper function signature
+    // ; function-name(arg/*type*/,...) /*UFUNCTION-flag[+another flag]*/
+    NewFunction(x/*int*/,y/*int*/) /*NetMulticast*/ {
+        console.log(x+y);
+    }
+}
+
+let MyActor_C = require('uclass')()(global,MyActor)
+
+let _ = require('lodash')
+
+function GetPC() {
+    return GWorld.GetAllActorsOfClass(PlayerController).OutActors[0]
+}
+
+function main() {
+    if (GWorld.IsServer()) {
+        let actor = new MyActor_C(GWorld,{X:1})
+
+        return function () {
+            actor.DestroyActor()
         }
+    } else {
+        return function() {}
     }
+}
 
-    try {
-        module.exports = () => {
-            let cleanup = null
-            process.nextTick(() => cleanup = main());
-            return () => cleanup()
-        }
+try {
+    module.exports = () => {
+        let cleanup = null
+        process.nextTick(() => cleanup = main());
+        return () => cleanup()
     }
-    catch (e) {
-        require('bootstrap')('helloBlueprint')
-    }
-})(this)
+}
+catch (e) {
+    require('bootstrap')('helloBlueprint')
+}
