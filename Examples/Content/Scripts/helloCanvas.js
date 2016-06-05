886cb8f130717628f8ae4f414df4931a7f3eb71a
Nako Sung <deif79@gmail.com>
2016-06-05 11:29:12 +0900

    Update helloCanvas.js

--- Examples/Content/Scripts/helloCanvas.js
+++ Examples/Content/Scripts/helloCanvas.js
@@ -1,63 +1,61 @@
-(function (global) {
-    "use strict"
+"use strict"
 
-    // find a local player controller
-    function GetPC() {
-        // out-ref function returns an object which contains out-ref params and return param.
-        // eg) bool USomeClass::Func(int a, int* b, int* c); -> {$:{bool}, b:{int}, c:{int}}
-        return PlayerController.C(GWorld.GetAllActorsOfClass(PlayerController).OutActors[0])
-    }
+// find a local player controller
+function GetPC() {
+    // out-ref function returns an object which contains out-ref params and return param.
+    // eg) bool USomeClass::Func(int a, int* b, int* c); -> {$:{bool}, b:{int}, c:{int}}
+    return PlayerController.C(GWorld.GetAllActorsOfClass(PlayerController).OutActors[0])
+}
 
-    function main() {
-        // set cursor type
-        GetPC().DefaultMouseCursor = Symbol('Default')
+function main() {
+    // set cursor type
+    GetPC().DefaultMouseCursor = Symbol('Default')
 
-        // declare our own HUD class
-        class MyHUD extends HUD {
-            ctor() {
-            }
-
-            // override ReceiveDrawHUD
-            ReceiveDrawHUD() {
-                // don't forget to its super function
-                super.ReceiveDrawHUD()
-
-                // have fun with Canvas
-                let text = `Hello Canvas : ${new Date().toISOString()}`
-
-                this.Canvas.DrawText(
-                    GEngine.SmallFont,
-                    text,
-                    {X:this.Canvas.SizeX/2,Y:this.Canvas.SizeY/2},
-                    {R:1,G:1,B:1,A:1},
-                    0,
-                    {R:0,G:0,B:0,A:1},
-                    {X:1,Y:1},
-                    true,true,true,
-                    {R:0,G:0,B:0,A:1}
-                )
-            }
+    // declare our own HUD class
+    class MyHUD extends HUD {
+        ctor() {
         }
 
-        // register new HUD class
-        let MyHUD_C = require('uclass')()(global,MyHUD);
+        // override ReceiveDrawHUD
+        ReceiveDrawHUD() {
+            // don't forget to its super function
+            super.ReceiveDrawHUD()
 
-        // and instantiate it
-        GetPC().ClientSetHUD(MyHUD_C)
+            // have fun with Canvas
+            let text = `Hello Canvas : ${new Date().toISOString()}`
 
-        // no mess to clean
-        return function () {
+            this.Canvas.DrawText(
+                GEngine.SmallFont,
+                text,
+                {X:this.Canvas.SizeX/2,Y:this.Canvas.SizeY/2},
+                {R:1,G:1,B:1,A:1},
+                0,
+                {R:0,G:0,B:0,A:1},
+                {X:1,Y:1},
+                true,true,true,
+                {R:0,G:0,B:0,A:1}
+            )
         }
     }
 
-    try {
-        module.exports = () => {
-            let cleanup = null
-            process.nextTick(() => cleanup = main());
-            return () => cleanup()
-        }
+    // register new HUD class
+    let MyHUD_C = require('uclass')()(global,MyHUD);
+
+    // and instantiate it
+    GetPC().ClientSetHUD(MyHUD_C)
+
+    // no mess to clean
+    return function () {
     }
-    catch (e) {
-        require('bootstrap')('helloCanvas')
+}
+
+try {
+    module.exports = () => {
+        let cleanup = null
+        process.nextTick(() => cleanup = main());
+        return () => cleanup()
     }
-})(this)
+}
+catch (e) {
+    require('bootstrap')('helloCanvas')
+}
