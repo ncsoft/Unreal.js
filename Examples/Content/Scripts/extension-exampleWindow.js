f1a9ee937ff14a1c9a8e9ddba9b76a3ac971cd39
Nako Sung <deif79@gmail.com>
2016-06-07 09:03:27 +0900

    Update extension-exampleWindow.js

--- Examples/Content/Scripts/extension-exampleWindow.js
+++ Examples/Content/Scripts/extension-exampleWindow.js
@@ -9,32 +9,32 @@ const svnPath = `${Context.GetDir('Engine')}/Binaries/ThirdParty/svn/Win64/svn`
 function main() {
     const UMG = require('UMG')
     let {EventEmitter} = require('events')
-    
+
     let font = {
         Size:10,
         FontObject:Root.GetEngine().SmallFont
     }
-    
+
     const I = require('instantiator')
     const _ = require('lodash')
     const request = require('request')
     const style = new JavascriptStyleSet
     style.StyleSetName = 'EditorStyle'
-    
+
     let workDir = Context.GetDir('GameContent')+'/Scripts/Downloaded'
-    
+
     function fetchGithub() {
         return request('GET',githubUrl)
     }
-    
+
     function fetchPackages() {
         return request("GET",repositoryUrl)
     }
-    
+
     function getNumClassesExported() {
         return _.filter(_.keys(global),x=>!!global[x].StaticClass).length
     }
-    
+
     function installPackage(pack) {
         return JavascriptProcess.Create(
             svnPath,
@@ -43,7 +43,7 @@ function main() {
             false,
             false,0,workDir).Wait()
     }
-    
+
     function packageToObject(p) {
         let o = new JavascriptObject()
         o.package = p
@@ -55,20 +55,22 @@ function main() {
         }
         return o
     }
-     
+
+    let commandList = JavascriptMenuLibrary.CreateUICommandList()
+
     makeWindow("$window",
     {
-        SizingRule:'AutoSized',  
-        Title:'Unreal.js' 
+        SizingRule:'AutoSized',
+        Title:'Unreal.js'
     })(finish => {
         let E = new EventEmitter()
-        return UMG(SizeBox,{WidthOverride:400},            
+        return UMG(SizeBox,{WidthOverride:400},
             UMG.div({Size:{Rule:'Fill'}},
                 UMG.span({},
                     UMG.text({},"Unreal.js"),
                     UMG.text({
                         Font:font,
-                        'Slot.Size.Rule':'Fill',                    
+                        'Slot.Size.Rule':'Fill',
                         'Slot.HorizontalAlignment':'HAlign_Right',
                         'Slot.VerticalAlignment':'VAlign_Center',
                         $link:elem => {
@@ -82,21 +84,90 @@ function main() {
                             elem.alive = false
                         }
                     })
-                ),            
+                ),
                 UMG.text({AutoWrapText:true,Font:font,'Slot.Padding':{Left:20,Top:10}},
-`Unreal.js is a plug-in which brings V8-powered Javascript into UnrealEngine4. 
+`Unreal.js is a plug-in which brings V8-powered Javascript into UnrealEngine4.
 
 Copyright (c) 2016 NCSOFT Corporation
 
 ${getNumClassesExported()} classes exported`
                 ),
                 UMG(SizeBox,{HeightOverride:20}),
-                UMG(SizeBox,{HeightOverride:200},                
+                UMG(SizeBox,{HeightOverride:100},
                     UMG(JavascriptListView,{
                         ItemHeight:20,
+                        Columns:[
+                            {
+                                Id: 'Name',
+                                Width: 0.7
+                            }
+                        ],
+                        OnGenerateRowEvent:item => {
+                            return I(
+                                UMG.text({Font:font},item ? item.target : 'Context')
+                            )
+                        },
+                        $link:elem => {
+                            elem.alive = true
+
+                            let old
+                            function refresh() {
+                                function fetch() {
+                                    let out
+                                    {
+                                        out = JavascriptLibrary.GetObjectsOfClass(JavascriptContext, [], false, 0x10).Results
+                                        out = out.map(x => x.GetDisplayName())
+                                        let cur = out.join(',')
+                                        if (cur != old) {
+                                            old = cur
+                                            out = out.map(x => {
+                                                let y = new JavascriptObject()
+                                                console.log(x)
+                                                y.target = x
+                                                return y
+                                            })
+                                        } else {
+                                            out = null
+                                        }
+                                    }
+                                    gc()
+                                    return out
+                                }
+                                let cur = fetch()
+                                if (cur) {
+                                    elem.Items = cur
+                                    elem.RequestListRefresh()
+                                }
+                            }
+
+                            function tick() {
+                                if (!elem.alive) return
+                                refresh()
+                                setTimeout(tick,1000)
+                            }
+
+                            tick()
+                        },
+                        $unlink:elem => {
+                            elem.alive = false
+                        }
+                    })
+                ),
+                UMG(SizeBox,{HeightOverride:200},
+                    UMG(JavascriptListView,{
+                        ItemHeight:20,
+                        OnContextMenuOpening:_ => {
+                            let widget
+                            commandList.CreateMenuBuilder(false,_builder => {
+                                let builder = JavascriptMenuBuilder.C(_builder)
+                                builder.BeginSection("test")
+                                builder.EndSection()
+                            })
+                            return null
+                        },
                         OnGenerateRowEvent:(item,column) => {
                             const isName = column == 'Name'
-                            return I(                                
+                            return I(
                                 UMG.text(
                                     {
                                         Font:font,
@@ -120,11 +191,11 @@ ${getNumClassesExported()} classes exported`
                         ],
                         $link:elem => {
                             elem.JavascriptContext = Context
-                            elem.alive = true                        
+                            elem.alive = true
                             elem.proxy = {
-                                OnDoubleClick : item => {                                
+                                OnDoubleClick : item => {
                                     JavascriptLibrary.MakeDirectory(workDir,false)
-                                    installPackage(item.package)                                    
+                                    installPackage(item.package)
                                 }
                             }
                             fetchPackages().then(repository => {
@@ -138,10 +209,10 @@ ${getNumClassesExported()} classes exported`
                         }
                     })
                 ),
-                
+
                 UMG(Spacer,{'Slot.Size.Rule' : 'Fill'}),
-                UMG(Button, 
-                    {   
+                UMG(Button,
+                    {
                         WidgetStyle: style.GetButtonStyle("Credits.Button"),
                         OnClicked: _ => {
                             JavascriptProcess.LaunchURL(homepageUrl)
@@ -149,25 +220,25 @@ ${getNumClassesExported()} classes exported`
                     },
                     UMG.text({Font:font},"Visit project page")
                 ),
-                UMG(Button, 
-                    {   
+                UMG(Button,
+                    {
                         WidgetStyle: style.GetButtonStyle("FlatButton.Dark"),
                         OnClicked: finish
                     },
-                    UMG.text({Font:font},"Close this window!") 
+                    UMG.text({Font:font},"Close this window!")
                 )
             )
         )
     })
-    
-    return _ => 0         
-}  
- 
+
+    return _ => 0
+}
+
 function makeWindow(key,opts) {
     const _ = require('lodash')
     const UMG = require('UMG')
     const I = require('instantiator')
-     
+
     if (!global[key]) {
         let window
         let container
@@ -176,22 +247,22 @@ function makeWindow(key,opts) {
                 {
                     $link:elem => window = elem
                 },opts),
-                UMG(SizeBox,{$link:elem => container = elem})           
-            )  
+                UMG(SizeBox,{$link:elem => container = elem})
+            )
         )
-        widget.TakeWidget().AddWindow()       
-         
+        widget.TakeWidget().AddWindow()
+
         let prev
         function add(child) {
             if (prev) {
-                container.remove_child(prev) 
+                container.remove_child(prev)
             }
             prev = container.add_child(child(finish))
-            process.nextTick(_ => window.BringToFront()) 
-        }       
-        
-        global[key] = add 
-         
+            process.nextTick(_ => window.BringToFront())
+        }
+
+        global[key] = add
+
         function finish() {
             if (window) {
                 window.RequestDestroyWindow()
@@ -200,8 +271,8 @@ function makeWindow(key,opts) {
             }
         }
     }
-    
-    return global[key] 
+
+    return global[key]
 }
 
 module.exports = function() {
@@ -219,5 +290,5 @@ module.exports = function() {
     } catch (e) {
         console.error(String(e),'got error')
         return _ => null
-    }    
+    }
 }
